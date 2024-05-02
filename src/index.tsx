981cd94f4d0fd94c679dc180e5bed5be1fa70e5e
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-05-02 03:31:26 -0700

    CSS Bundling, Initial Progress

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import "jquery";
 import "bootstrap/dist/js/bootstrap.min.js";
 import { render } from "solid-js/web";
 import App from "./App.js";
+import "./index.css";
 
 const root: HTMLDivElement = document.querySelector("#root")!;
 
