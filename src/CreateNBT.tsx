ffe80569627b193374d7ea0429ae6e101e14254d
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-05-02 03:23:09 -0700

    Platform Component
    The styles aren't 100% perfectly the same, I want to write them from scratch with a plain CSS file anyways, so that will fix things again.

--- src/CreateNBT.tsx
+++ src/CreateNBT.tsx
@@ -14,49 +14,34 @@ export default function CreateNBT() {
         <>
             <h2 class="text-center" style="margin-bottom: 30px;color: white;margin-top: 50px;font-weight: bold;">Select A Platform!</h2>
             <div class="col text-center" style="background: rgb(35,40,50);border-top: 2px solid rgb(75,80,90) ;">
-                <button class="btn btn-primary" type="button" style="background: rgb(55,60,70);margin-top: 15px;color: white;margin-bottom: 15px;margin-right: 5px;margin-left: 5px;width: 190px;height: 190px;border: 2px solid rgb(75,80,90);border-radius: 15px;font-size: 14px;">
-                    <img class="justify-content-center align-items-center align-content-center align-self-center" style="width: 70px;height: 70px;margin-right: 40px;margin-left: 40px;margin-bottom: 15px;" src={Java_Platform_Icon}/>
-                    &nbsp;Java
-                </button>
-                <button class="btn btn-primary" type="button" style="background: rgb(55,60,70);margin-top: 15px;color: white;margin-bottom: 15px;margin-right: 5px;margin-left: 5px;width: 190px;height: 190px;border: 2px solid rgb(75,80,90);border-radius: 15px;font-size: 14px;">
-                    <img class="justify-content-center align-items-center align-content-center align-self-center" style="width: 70px;height: 70px;margin-right: 40px;margin-left: 40px;margin-bottom: 15px;" src={Bedrock_Platform_Icon}/>
-                    &nbsp;Bedrock
-                </button>
-                <button class="btn btn-primary" type="button" style="background: rgb(55,60,70);margin-top: 15px;color: white;margin-bottom: 15px;margin-right: 5px;margin-left: 5px;width: 190px;height: 190px;border: 2px solid rgb(75,80,90);border-radius: 15px;font-size: 14px;">
-                    <img class="justify-content-center align-items-center align-content-center align-self-center" style="width: 70px;height: 70px;margin-right: 40px;margin-left: 40px;margin-bottom: 15px;" src={Xbox_360_Platform_Icon}/>
-                    &nbsp;Xbox 360
-                </button>
-                <button class="btn btn-primary" type="button" style="background: rgb(55,60,70);margin-top: 15px;color: white;margin-bottom: 15px;margin-right: 5px;margin-left: 5px;width: 190px;height: 190px;border: 2px solid rgb(75,80,90);border-radius: 15px;font-size: 14px;">
-                    <img class="justify-content-center align-items-center align-content-center align-self-center" style="width: 90px;height: 90px;margin-right: 20px;margin-left: 20px;margin-bottom: 15px;" src={PlayStation_3_Platform_Icon}/>
-                    &nbsp;PlayStation 3
-                </button>
-                <button class="btn btn-primary" type="button" style="background: rgb(55,60,70);margin-top: 15px;color: white;margin-bottom: 15px;margin-right: 5px;margin-left: 5px;width: 190px;height: 190px;border: 2px solid rgb(75,80,90);border-radius: 15px;font-size: 14px;">
-                    <img class="justify-content-center align-items-center align-content-center align-self-center" style="width: 70px;height: 70px;margin-right: 40px;margin-left: 40px;margin-bottom: 15px;" src={Wii_U_Platform_Icon}/>
-                    &nbsp;Wii U
-                </button>
+                <Platform name="Java" src={Java_Platform_Icon} size={70}/>
+                <Platform name="Bedrock" src={Bedrock_Platform_Icon} size={70}/>
+                <Platform name="Xbox 360" src={Xbox_360_Platform_Icon} size={70}/>
+                <Platform name="PlayStation 3" src={PlayStation_3_Platform_Icon} size={90}/>
+                <Platform name="Wii U" src={Wii_U_Platform_Icon} size={70}/>
             </div>
             <div class="col text-center" style="background: rgb(35,40,50);border-bottom: 2px solid rgb(75,80,90) ;">
-                <button class="btn btn-primary" type="button" style="background: rgb(55,60,70);color: white;margin-bottom: 15px;margin-right: 5px;margin-left: 5px;width: 190px;height: 190px;border: 2px solid rgb(75,80,90);border-radius: 15px;font-size: 14px;">
-                    <img class="justify-content-center align-items-center align-content-center align-self-center" style="width: 70px;height: 70px;margin-right: 40px;margin-left: 40px;margin-bottom: 15px;" src={New_3DS_Platform_Icon}/>
-                    &nbsp;New Nintendo 3DS
-                </button>
-                <button class="btn btn-primary" type="button" style="background: rgb(55,60,70);color: white;margin-bottom: 15px;margin-right: 5px;margin-left: 5px;width: 190px;height: 190px;border: 2px solid rgb(75,80,90);border-radius: 15px;font-size: 14px;">
-                    <img class="justify-content-center align-items-center align-content-center align-self-center" style="width: 100px;height: 100px;margin-right: 10px;margin-left: 10px;margin-bottom: 15px;" src={PlayStation_Vita_Platform_Icon}/>
-                    &nbsp;PlayStation Vita
-                </button>
-                <button class="btn btn-primary" type="button" style="background: rgb(55,60,70);color: white;margin-bottom: 15px;margin-right: 5px;margin-left: 5px;width: 190px;height: 190px;border: 2px solid rgb(75,80,90);border-radius: 15px;font-size: 14px;">
-                    <img class="justify-content-center align-items-center align-content-center align-self-center" style="width: 70px;height: 70px;margin-right: 40px;margin-left: 40px;margin-bottom: 15px;" src={Xbox_One_Platform_Icon}/>
-                    &nbsp;Xbox One
-                </button>
-                <button class="btn btn-primary" type="button" style="background: rgb(55,60,70);color: white;margin-bottom: 15px;margin-right: 5px;margin-left: 5px;width: 190px;height: 190px;border: 2px solid rgb(75,80,90);border-radius: 15px;font-size: 14px;">
-                    <img class="justify-content-center align-items-center align-content-center align-self-center" style="width: 90px;height: 90px;margin-right: 20px;margin-left: 20px;margin-bottom: 15px;" src={PlayStation_4_Platform_Icon}/>
-                    &nbsp;PlayStation 4
-                </button>
-                <button class="btn btn-primary" type="button" style="background: rgb(55,60,70);color: white;margin-bottom: 15px;margin-right: 5px;margin-left: 5px;width: 190px;height: 190px;border: 2px solid rgb(75,80,90);border-radius: 15px;font-size: 14px;">
-                    <img class="justify-content-center align-items-center align-content-center align-self-center" style="width: 70px;height: 70px;margin-right: 40px;margin-left: 40px;margin-bottom: 15px;" src={Nintendo_Switch_Platform_Icon}/>
-                    &nbsp;Switch
-                </button>
+                <Platform name="New Nintendo 3DS" src={New_3DS_Platform_Icon} size={70}/>
+                <Platform name="PlayStation Vita" src={PlayStation_Vita_Platform_Icon} size={100}/>
+                <Platform name="Xbox One" src={Xbox_One_Platform_Icon} size={70}/>
+                <Platform name="PlayStation 4" src={PlayStation_4_Platform_Icon} size={90}/>
+                <Platform name="Switch" src={Nintendo_Switch_Platform_Icon} size={70}/>
             </div>
         </>
+    );
+}
+
+interface PlatformProps {
+    name: string;
+    src: string;
+    size: number;
+}
+
+function Platform(props: PlatformProps) {
+    return (
+        <button class="btn btn-primary" type="button" style="background: rgb(55,60,70);color: white;margin-bottom: 15px;margin-right: 5px;margin-left: 5px;width: 190px;height: 190px;border: 2px solid rgb(75,80,90);border-radius: 15px;font-size: 14px;">
+            <img class="justify-content-center align-items-center align-content-center align-self-center" style={`width: ${props.size}px;height: ${props.size}px;margin-right: 40px;margin-left: 40px;margin-bottom: 15px;`} src={props.src}/>
+            &nbsp;{props.name}
+        </button>
     );
 }
