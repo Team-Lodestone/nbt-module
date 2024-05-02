9237c6519c072b56b525a5effc32d2ffc32a49d6
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-05-02 02:49:09 -0700

    Page Navigation
    Wow! This works great.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
+import { Match, Switch, createSignal } from "solid-js";
+import CreateNBT from "./CreateNBT.js";
 import Main from "./Main.js";
+import NBTEditor from "./NBTEditor.js";
+
+export type Page = "main" | "create-nbt" | "nbt-editor";
+
+export const [page, setPage] = createSignal<Page>("main");
 
 export default function App() {
   return (
     <>
-      <Main/>
+      <Switch>
+        <Match when={page() === "main"}>
+          <Main/>
+        </Match>
+        <Match when={page() === "create-nbt"}>
+          <CreateNBT/>
+        </Match>
+        <Match when={page() === "nbt-editor"}>
+          <NBTEditor/>
+        </Match>
+      </Switch>
     </>
   );
 }
--- src/Main.tsx
+++ src/Main.tsx
@@ -1,3 +1,5 @@
+import { setPage } from "./App.js";
+
 export default function Main() {
     return (
         <>
@@ -5,7 +7,7 @@ export default function Main() {
                 <h2 class="text-center" style="margin-bottom: 30px;color: white;margin-top: 50px;font-weight: bold;">NBT Editor</h2>
             </div>
             <div class="col text-center" style="background: rgb(35,40,50);border-top: 2px solid rgb(75,80,90) ;border-bottom: 2px solid rgb(75,80,90) ;">
-                <button class="btn btn-primary" type="button" style="background: rgb(55,60,70);margin-top: 15px;color: white;margin-bottom: 15px;margin-right: 5px;margin-left: 5px;width: 190px;height: 190px;font-size: 14px;border: 2px solid rgb(75,80,90);border-radius: 15px;">
+                <button class="btn btn-primary" type="button" style="background: rgb(55,60,70);margin-top: 15px;color: white;margin-bottom: 15px;margin-right: 5px;margin-left: 5px;width: 190px;height: 190px;font-size: 14px;border: 2px solid rgb(75,80,90);border-radius: 15px;" onclick={() => setPage("nbt-editor")}>
                     <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="1em" viewBox="0 0 24 24" width="1em" fill="currentColor" class="justify-content-center align-items-center align-content-center align-self-center icon" style="color: rgb(250,250,250);width: 70px;height: 70px;margin-right: 40px;margin-left: 40px;margin-bottom: 15px;">
                         <g>
                             <rect fill="none" height="24" width="24"></rect>
@@ -16,7 +18,7 @@ export default function Main() {
                     </svg>
                     &nbsp;Open World / NBT File
                 </button>
-                <button class="btn btn-primary" type="button" style="background: rgb(55,60,70);margin-top: 15px;color: white;margin-bottom: 15px;margin-right: 5px;margin-left: 5px;width: 190px;height: 190px;border: 2px solid rgb(75,80,90);border-radius: 15px;/*font-size: 14px;*/">
+                <button class="btn btn-primary" type="button" style="background: rgb(55,60,70);margin-top: 15px;color: white;margin-bottom: 15px;margin-right: 5px;margin-left: 5px;width: 190px;height: 190px;border: 2px solid rgb(75,80,90);border-radius: 15px;/*font-size: 14px;*/" onclick={() => setPage("create-nbt")}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-plus-lg justify-content-center align-items-center align-content-center align-self-center icon" style="color: rgb(250,250,250);width: 70px;height: 70px;margin-right: 40px;margin-left: 40px;margin-bottom: 15px;">
                         <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"></path>
                     </svg>
